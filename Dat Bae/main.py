edd5d1918e6c36a29e21e44f8f5d973bc1d59651
4m1g0 <dev.4m1g0@gmail.com>
2019-04-10 21:03:28 +0200

    Succesfully solved Dat Bae

--- Dat Bae/main.py
+++ Dat Bae/main.py
@@ -1,93 +1,41 @@
-import itertools
-from math import ceil
 
-def getTestMsg(N, block):
-    currentBSize = 0
-    currentBSimbol = '0'
-    testMsg = ''
-    
-    for i in range(N):
-        testMsg += currentBSimbol
-        currentBSize += 1
-        
-        if currentBSize >= block:
-            currentBSize = 0
-            if currentBSimbol == '1':
-                currentBSimbol = '0'
-            else:
-                currentBSimbol = '1'
-                
-    return testMsg
-     
-def getBlockRealSize(blockNumber, blockSize, msg):
-    currentBSize = 0
-    currentBSimbol = '0'
-    workingNodes = []
-    
-    for s in msg:
-        if s != currentBSimbol:
-            if currentBSimbol == '1':
-                currentBSimbol = '0'
-            else:
-                currentBSimbol = '1'
-            workingNodes.append(currentBSize)
-            currentBSize = 0
-            
-        currentBSize += 1
-    workingNodes.append(currentBSize)
-    if len(workingNodes) < blockNumber: # last block could be totally missing
-        workingNodes.append(0)
-        
-    return workingNodes
     
 
 def solve(t):
-    N,B,F = input().split(' ')
+    N,B,F = map(int, input().split(' '))
     N = int(N)
     
-    block = 16
-    if N < 32:
-        block = N/2
+    tc = [[0 for __ in range(N)] for __ in range(5)]
     
-    blockLength = []
+    for i in range(5):
+        for j in range(N):
+            tc[i][j] = ((j & 31) >> i) & 1
+    rc = []
+    for i,t in enumerate(tc):
+        print(''.join(map(str, t)))
+        rc.append(list(map(int, input())))
     
-    testMsg = getTestMsg(N, block)
-    print(testMsg)
-    result = input()
+    #print(rc)
+    errors = []
+    ti = 0
+    for i in range(N-B):
+        fail = True
+        while fail:
+            fail = False
+            for j in range(5):
+                if tc[j][ti] != rc[j][i]:
+                    errors.append(str(ti))
+                    fail = True
+                    break
+            ti += 1
+            
+    for i in range(ti, N):
+        errors.append(str(i))
+                
+    print(' '.join(errors))
     
-    workingNodes = getBlockRealSize(N/block, block, result)
-    
+    assert(int(input()) == 1)
         
-    #print(workingNodes)
-    
-    # TODO: test = 2 FINISH
-    
-    while block > 1:
-        block /= 2
-        testMsg = getTestMsg(N, block)
-        print(testMsg)
-        
-        result = input()
-        currPos = 0
-        
-        for i, x in enumerate(workingNodes):
-            workingNodes[i] = getBlockRealSize(2, block, result[currPos:currPos+x])
-            currPos += x
-         
-        #print(workingNodes)
-        workingNodes = list(itertools.chain.from_iterable(workingNodes))
-        #print(workingNodes)
-    
-    answer = ''
-    for i in range(N):
-        if workingNodes[i] == 0:
-            answer += str(i) + " "
-    
-    answer = answer.strip()
-    
-    print(answer)
-    response = input()
-     
 
 T = int(input())
 for t in range(T):
